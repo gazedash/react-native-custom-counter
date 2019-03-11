cd11d4f4a4d1af9adfccad2f8c20268a1389647c
Andrey <andrey@Andreys-MacBook-Pro.local>
2019-03-11 10:30:07 +0300

    Remove unused methods

--- ios/CounterView.swift
+++ ios/CounterView.swift
@@ -1,23 +1,7 @@
-//
-//  CounterView.swift
-//  CounterApp
-//
-//  Created by Andrei Pfeiffer on 3/29/18.
-//
-
 import UIKit
 import MediaPlayer
 
 class CounterView: UIView {
-
-  @objc var count: NSNumber = 0 {
-    didSet {
-      button.setTitle(String(describing: count), for: .normal)
-    }
-  }
-  
-  @objc var onUpdate: RCTDirectEventBlock?
-
   override init(frame: CGRect) {
     super.init(frame: frame)
     self.addSubview(button)
@@ -37,43 +21,10 @@ class CounterView: UIView {
       action: #selector(increment),
       for: .touchUpInside
     )
-    let longPress = UILongPressGestureRecognizer(
-      target: self,
-      action: #selector(sendUpdate(_:))
-    )
-    b.addGestureRecognizer(longPress)
     return b
   }()
-
-  @objc func increment() {
-
-    // let rect = CGRect(x: 10, y: 10, width: 100, height: 100) 
-    // let airplayVolume = MPVolumeView(frame: rect)
-    // airplayVolume.showsVolumeSlider = true
-    // airplayVolume.showsRouteButton = true
-    // self.addSubview(airplayVolume)
-    // for view: UIView in airplayVolume.subviews {
-    //     if let button = view as? UIButton {
-    //         button.sendActions(for: .touchUpInside)
-    //         break
-    //     }
-    // }
-
-    count = count.intValue + 1 as NSNumber
-  }
-  
-  @objc func sendUpdate(_ gesture: UILongPressGestureRecognizer) {
-    if gesture.state == .began {
-      if onUpdate != nil {
-        // our Event Emitter expects [AnyHashable:Any]
-        onUpdate!(["count": count])
-      }
-    }
-  }
   
   @objc func update(value: NSNumber) {
-    print("was i called")
-
     let rect = CGRect(x: 10, y: 10, width: 100, height: 100) 
     let airplayVolume = MPVolumeView(frame: rect)
     airplayVolume.showsVolumeSlider = true
@@ -85,8 +36,6 @@ class CounterView: UIView {
             break
         }
     }
-
-    count = value
   }
 
 }
--- ios/CounterViewManager.swift
+++ ios/CounterViewManager.swift
@@ -8,25 +8,11 @@ class CounterViewManager: RCTViewManager {
   }
   
   @objc func updateFromManager(_ node: NSNumber, count: NSNumber) {
-    // let rect = CGRect(x: -100, y: 0, width: 0, height: 0)
-    // let rect = CGRect(x: 0, y: 0, width: 100, height: 100) 
-    // let airplayVolume = MPVolumeView(frame: rect)
-    // airplayVolume.showsVolumeSlider = true
-    // airplayVolume.showsRouteButton = true
-    // self.view().addSubview(airplayVolume)
-    // for view: UIView in airplayVolume.subviews {
-    //     if let button = view as? UIButton {
-    //         button.sendActions(for: .touchUpInside)
-    //         break
-    //     }
-    // }
-
     DispatchQueue.main.async {
       let component = self.bridge.uiManager.view(
         forReactTag: node
       ) as! CounterView
       component.update(value: count)
     }
-    // airplayVolume.removeFromSuperview()
   }
 }
