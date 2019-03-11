c29cc3eea6218670f4e17601c2874086912aaf98
Andrey <andrey@Andreys-MacBook-Pro.local>
2019-03-11 10:30:50 +0300

    Remove more unused methods

--- ios/CounterView.swift
+++ ios/CounterView.swift
@@ -4,25 +4,11 @@ import MediaPlayer
 class CounterView: UIView {
   override init(frame: CGRect) {
     super.init(frame: frame)
-    self.addSubview(button)
-    increment()
   }
 
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
-  lazy var button: UIButton = {
-    let b = UIButton.init(type: UIButton.ButtonType.system)
-    b.titleLabel?.font = UIFont.systemFont(ofSize: 50)
-    b.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    b.addTarget(
-      self,
-      action: #selector(increment),
-      for: .touchUpInside
-    )
-    return b
-  }()
   
   @objc func update(value: NSNumber) {
     let rect = CGRect(x: 10, y: 10, width: 100, height: 100) 
@@ -37,5 +23,4 @@ class CounterView: UIView {
         }
     }
   }
-
 }
